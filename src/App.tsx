7193549cb63cd6ec7b6c5b586a41dde87d739942
bspuneeth <puneethbsgithub@gmail.com>
2025-01-22 19:03:48 -0500

    fixed error

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,14 @@ import Kambaz from "./Kambaz";
 import Labs from "./Labs";
 import { HashRouter, Navigate, Route, Routes } from "react-router-dom";
 export default function App() {
-  return (
-    <HashRouter>
-      <div>
-        <Routes>
-          <Route path="/" element={<Labs />} />
-          <Route path="/Labs/*" element={<Labs />} />
-          <Route path="/Kambaz/*" element={<Kambaz />} />
-        </Routes>
-      </div>
-    </HashRouter>
-  );
-}
+ return (
+  <HashRouter>
+   <div>
+    <Routes>
+     <Route path="/" element={<Navigate to="labs/Lab1"/>}/>
+     <Route path="/Labs/*" element={<Labs />} />
+     <Route path="/Kambaz/*" element={<Kambaz />} />
+    </Routes>
+   </div>
+  </HashRouter>
+);}
