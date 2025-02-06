339e8f0bbb034db7d208447736319097b5858bc7
bspuneeth <puneethbsgithub@gmail.com>
2025-02-05 22:11:42 -0500

    a2 CSS and Bootstrap

--- src/Kambaz/Account/Navigation.tsx
+++ src/Kambaz/Account/Navigation.tsx
@@ -1,15 +1,30 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import "../styles.css"; 
 
 export default function AccountNavigation() {
   return (
     <div id="wd-account-navigation" className="wd list-group fs-5 rounded-0">
-      <Link to="/Kambaz/Account/Signin"
-        className="list-group-item text-danger border border-0"> Signin </Link>
-      <Link to="/Kambaz/Account/Signup"
-        className="list-group-item text-danger border border-0"> Signup </Link>
-      <Link to="/Kambaz/Account/Profile"
-        className="list-group-item text-danger border border-0"> Profile </Link>
+      <NavLink 
+        to="/Kambaz/Account/Signin"
+        id="wd-account-signin-link"
+        className="list-group-item border border-0 text-danger"
+      >
+        Signin
+      </NavLink>
+      <NavLink 
+        to="/Kambaz/Account/Signup"
+        id="wd-account-signup-link"
+        className="list-group-item border border-0 text-danger"
+      >
+        Signup
+      </NavLink>
+      <NavLink 
+        to="/Kambaz/Account/Profile"
+        id="wd-account-profile-link"
+        className="list-group-item border border-0 text-danger"
+      >
+        Profile
+      </NavLink>
     </div>
   );
 }
--- src/Kambaz/Courses/Navigation.tsx
+++ src/Kambaz/Courses/Navigation.tsx
@@ -1,22 +1,58 @@
-import { Link } from "react-router-dom";
-import "../styles.css"; 
+import { NavLink } from "react-router-dom";
+import "../styles.css";
+
 export default function CourseNavigation() {
   return (
     <div id="wd-courses-navigation" className="wd list-group fs-5 rounded-0">
-      <Link to="/Kambaz/Courses/1234/Home" id="wd-course-home-link"
-        className="list-group-item active border border-0"> Home </Link>
-      <Link to="/Kambaz/Courses/1234/Modules" id="wd-course-modules-link"
-        className="list-group-item text-danger border border-0"> Modules </Link>
-      <Link to="/Kambaz/Courses/1234/Piazza" id="wd-course-piazza-link"
-        className="list-group-item text-danger border border-0"> Piazza </Link>
-      <Link to="/Kambaz/Courses/1234/Zoom" id="wd-course-zoom-link"
-        className="list-group-item text-danger border border-0"> Zoom </Link>
-      <Link to="/Kambaz/Courses/1234/Assignments" id="wd-course-quizzes-link"
-        className="list-group-item text-danger border border-0"> Assignments </Link>
-      <Link to="/Kambaz/Courses/1234/Quizzes" id="wd-course-assignments-link"
-        className="list-group-item text-danger border border-0"> Quizzes </Link>
-      <Link to="/Kambaz/Courses/1234/People" id="wd-course-people-link"
-        className="list-group-item text-danger border border-0" > People </Link>
+      <NavLink
+        to="/Kambaz/Courses/1234/Home"
+        id="wd-course-home-link"
+        className="list-group-item border border-0"
+      >
+        Home
+      </NavLink>
+      <NavLink
+        to="/Kambaz/Courses/1234/Modules"
+        id="wd-course-modules-link"
+        className="list-group-item border border-0 text-danger"
+      >
+        Modules
+      </NavLink>
+      <NavLink
+        to="/Kambaz/Courses/1234/Piazza"
+        id="wd-course-piazza-link"
+        className="list-group-item border border-0 text-danger"
+      >
+        Piazza
+      </NavLink>
+      <NavLink
+        to="/Kambaz/Courses/1234/Zoom"
+        id="wd-course-zoom-link"
+        className="list-group-item border border-0 text-danger"
+      >
+        Zoom
+      </NavLink>
+      <NavLink
+        to="/Kambaz/Courses/1234/Assignments"
+        id="wd-course-assignments-link"
+        className="list-group-item border border-0 text-danger"
+      >
+        Assignments
+      </NavLink>
+      <NavLink
+        to="/Kambaz/Courses/1234/Quizzes"
+        id="wd-course-quizzes-link"
+        className="list-group-item border border-0 text-danger"
+      >
+        Quizzes
+      </NavLink>
+      <NavLink
+        to="/Kambaz/Courses/1234/People"
+        id="wd-course-people-link"
+        className="list-group-item border border-0 text-danger"
+      >
+        People
+      </NavLink>
     </div>
-);}
-
+  );
+}
